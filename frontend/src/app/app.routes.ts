d23ad8ee2ea2fa336146e2bb52e26e5e217ae7a1
Chokladglasyr <lim_ida@live.com>
2025-05-01 10:21:25 +0200

    feat: added paths and component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { ListsComponent } from './components/lists/lists.component';
 import { ItemsComponent } from './components/items/items.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchByUserComponent } from './components/search-by-user/search-by-user.component';
+import { SearchItemsComponent } from './components/search-items/search-items.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'lists', component: ListsComponent},
-    {path: 'items/:listid', component: ItemsComponent}
+    {path: 'items/:listid', component: ItemsComponent},
+    {path: 'search', component: SearchByUserComponent},
+    {path: 'searchprice', component: SearchItemsComponent}
 ];
